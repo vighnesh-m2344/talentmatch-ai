27299316b32b467231edd473729ef208740e436c
Vighnesh <vighnesh@gmail.com>
2026-04-19 20:49:34 +0530

    refactor: clean monorepo structure (backend + frontend separated)

--- prisma/migrations/20260411082556_add_role/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Role" AS ENUM ('USER', 'RECRUITER', 'ADMIN');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER';
--- prisma/migrations/20260416163130_package_as_string/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `salary` on the `Job` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Job" DROP COLUMN "salary",
-ADD COLUMN     "package" TEXT;
